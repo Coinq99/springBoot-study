4e24b51fd262aa2be1fa011e65bb74d69e8c4332
coinq99 <coinq@naver.com>
2023-02-06 14:30:19 +0900

    4.4 까지 학습 완

--- src/main/java/org/example/domain/posts/PostsRepository.java
+++ src/main/java/org/example/domain/posts/PostsRepository.java
@@ -1,6 +1,17 @@
 package org.example.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    //SpringDataJpa에서 제공하지 않는 메소드는 아래와 같이 쿼리(@Query)로 작성해도 됨을 보여주는 예시
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
+    /**위 쿼리는 예제일 뿐 SpringDataJpa에서 제공하는 기본 메소드 만으로 해결할 수 있음 다만 가독성 측면에서
+     * @Query를 사용하는 것이 훨씬 가독성이 좋으니 선택해서 사용하도록 하자
+     */
 }
--- src/main/java/org/example/web/IndexController.java
+++ src/main/java/org/example/web/IndexController.java
@@ -1,15 +1,27 @@
 package org.example.web;
 
+import lombok.RequiredArgsConstructor;
+import org.example.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
+    /** index() 매서드 수정으로인한 주석처리
+     * public String index() {
+        return "index";
+    }
+    */
 
     @GetMapping("/posts/save")
     public String postsSave(){
