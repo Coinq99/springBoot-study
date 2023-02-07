9e863c87879e658cb0f27256776027f20c968e48
coinq99 <coinq@naver.com>
2023-02-07 15:54:10 +0900

    챕터 5.3 까지 학습 완료

--- src/main/java/org/example/web/IndexController.java
+++ src/main/java/org/example/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.config.auth.dto.SessionUser;
 import org.example.service.posts.PostsService;
 import org.example.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession; //로그인 기능을 위한 추가
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        //로그인 기능관련 부분 추가
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
     /** index() 매서드 수정으로인한 주석처리
